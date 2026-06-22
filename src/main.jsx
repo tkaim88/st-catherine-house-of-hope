9e935b23907e083885f51dc6c80669a10c3440b5
tkaim88 <tkomoruh@gmail.com>
2026-06-22 17:55:55 +0300

    Create design system stylesheet structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
 import './styles/variables.css'
 import './styles/global.css'
+import './styles/layout.css'
+import './styles/components.css'
+import './styles/forms.css'
+import './styles/dashboard.css'
+import './styles/utilities.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -10,5 +17,5 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
